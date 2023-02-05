20a9be9e908b322b76c1e02a6c4f1cf051ec2580
Cynthia Bluteau <>
2023-02-04 19:40:03 -0500

    Installed utterances on repo

---
+++ adv/site/extras/js/utterance_sb.js
@@ -0,0 +1,8 @@
+<script src="https://utteranc.es/client.js"
+        repo="bluteau-cy/scor-atomix-adv"
+        issue-term="title"
+        theme="preferred-color-scheme"
+        crossorigin="anonymous"
+        async>
+</script>
+
